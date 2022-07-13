203bf5bdfd676087fc3768d3913a0738a1f63b1d
PhilipDW183 <philip.davidwilkinson@gmail.com>
2022-07-13 11:53:30 +0100

    Create change quality function

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -9,21 +9,24 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name == self.brie and item.quality < 50:
-                item.quality = item.quality + 1
-            elif item.name == self.backstage_pass and item.quality < 50:
-                item.quality = item.quality + 1
-                if item.sell_in < 11 and item.quality < 50:
-                    item.quality = item.quality + 1
-                if item.sell_in < 6 and item.quality < 50:
-                    item.quality = item.quality + 1
-            elif item.name != self.ragnaros and item.quality > 0:
-                item.quality = item.quality - 1
+            self.change_item_quality(item)
 
             self.decrease_sell_in(item)
 
             if item.sell_in < 0:
                 self.handle_passed_sell_by_date(item)
+
+    def change_item_quality(self, item):
+        if item.name == self.brie and item.quality < 50:
+            item.quality = item.quality + 1
+        elif item.name == self.backstage_pass and item.quality < 50:
+            item.quality = item.quality + 1
+            if item.sell_in < 11 and item.quality < 50:
+                item.quality = item.quality + 1
+            if item.sell_in < 6 and item.quality < 50:
+                item.quality = item.quality + 1
+        elif item.name != self.ragnaros and item.quality > 0:
+            item.quality = item.quality - 1
 
     def handle_passed_sell_by_date(self, item):
         if item.name == self.brie and item.quality < 50:
