f0d681db038246a363baac43c2ac669834237afa
PhilipDW183 <philip.davidwilkinson@gmail.com>
2022-07-13 11:51:58 +0100

    Refactor initial quality changing logic

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -9,16 +9,16 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != self.brie and item.name != self.backstage_pass:
-                if item.quality > 0 and item.name != self.ragnaros:
-                    item.quality = item.quality - 1
-            elif item.quality < 50:
+            if item.name == self.brie and item.quality < 50:
                 item.quality = item.quality + 1
-                if item.name == self.backstage_pass and item.quality < 50:
-                    if item.sell_in < 11:
-                        item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        item.quality = item.quality + 1
+            elif item.name == self.backstage_pass and item.quality < 50:
+                item.quality = item.quality + 1
+                if item.sell_in < 11 and item.quality < 50:
+                    item.quality = item.quality + 1
+                if item.sell_in < 6 and item.quality < 50:
+                    item.quality = item.quality + 1
+            elif item.name != self.ragnaros and item.quality > 0:
+                item.quality = item.quality - 1
 
             self.decrease_sell_in(item)
 
