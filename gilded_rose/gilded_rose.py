bb4aa625fcab607bacac8da51a7f300dc10684f0
PhilipDW183 <philip.davidwilkinson@gmail.com>
2022-07-13 11:16:41 +0100

    Remove one layer of nested if statements

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -11,29 +11,24 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             if item.name != self.brie and item.name != self.backstage_pass:
-                if item.quality > 0:
-                    if item.name != self.ragnaros:
-                        item.quality = item.quality - 1
+                if item.quality > 0 and item.name != self.ragnaros:
+                    item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
                     if item.name == self.backstage_pass:
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                        if item.sell_in < 11 and item.quality < 50:
+                            item.quality = item.quality + 1
+                        if item.sell_in < 6 and item.quality < 50:
+                            item.quality = item.quality + 1
             if item.name != self.ragnaros:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
                 if item.name != self.brie:
                     if item.name != self.backstage_pass:
-                        if item.quality > 0:
-                            if item.name != self.ragnaros:
-                                item.quality = item.quality - 1
+                        if item.quality > 0 and item.name != self.ragnaros:
+                            item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                elif item.quality < 50:
+                    item.quality = item.quality + 1
