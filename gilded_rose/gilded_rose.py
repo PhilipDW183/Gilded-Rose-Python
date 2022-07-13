68a2ab8da4cb5a4690ce390441c72e1711004459
PhilipDW183 <philip.davidwilkinson@gmail.com>
2022-07-13 11:07:03 +0100

    Replace strings with constant

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -3,29 +3,34 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    backstage_pass = "Backstage passes to a TAFKAL80ETC concert"
+    ragnaros = "Sulfuras, Hand of Ragnaros"
+    brie = "Aged Brie"
+
+
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.name != self.brie and item.name != self.backstage_pass:
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+                    if item.name != self.ragnaros:
                         item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.name == self.backstage_pass:
                         if item.sell_in < 11:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name != self.ragnaros:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.name != self.brie:
+                    if item.name != self.backstage_pass:
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                            if item.name != self.ragnaros:
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
